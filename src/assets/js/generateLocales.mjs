c921b37060f15d562c6218386383a94a03cdad6d
tomrcc <tom@cloudcannon.com>
2025-04-02 09:27:14 +1300

    Finish basic generateLocales file

--- src/assets/js/generateLocales.mjs
+++ src/assets/js/generateLocales.mjs
@@ -22,12 +22,14 @@ import path from "path";
     })
   );
 
-  // Preserve old keys and their translations, while adding new keys to be translated
   const baseJsonKeys = Object.keys(baseJsonData.keys);
   const existingLocales = Object.keys(localesFilesData);
+  // Use the first locale to find the keys currently in locales files - they should all have the same keys to translate
   const existingLocaleKeys = Object.keys(localesFilesData[existingLocales[0]]);
 
-  // Loop through each locale and update our data
+  // Preserve old keys and their translations
+  // When adding new keys to be translated use their original phrase as a placeholder so the content isn't empty
+  // For each locale, loop through they keys in our base.json and add any new ones to the locale files
   await Promise.all(
     existingLocales.map(async (locale) => {
       const localeFilePath = path.join(localesDirPath, `${locale}.json`);
@@ -35,6 +37,7 @@ import path from "path";
       // For each locale go through the base.json keys and add them to write if they're not in there already
       baseJsonKeys.map((baseJsonKey) => {
         if (!existingLocaleKeys.includes(baseJsonKey)) {
+          console.log(`Found key ${baseJsonKey} to add to locales!`);
           localeFileData[baseJsonKey] = {
             original: baseJsonData.keys[baseJsonKey].original,
             value: baseJsonData.keys[baseJsonKey].original,
@@ -42,19 +45,29 @@ import path from "path";
         }
       });
 
-      // Similarly, for each locale delete any keys to write in the locales that don't exist in the base.json because they're no longer used
+      // For each locale, delete any keys to write in the locales that don't exist in the base.json
       existingLocaleKeys.map((existingKey) => {
         if (!baseJsonKeys.includes(existingKey)) {
+          console.log(
+            `Key ${existingKey} no longer exists in our base.json - deleting from locales now...`
+          );
           delete localeFileData[existingKey];
         }
+      });
+
+      const localeDataToWrite = {};
+      // Sort the locales file so that it doesn't change randomly each build
+      const keysForLocale = Object.keys(localeFileData);
+      keysForLocale.sort().map((key) => {
+        localeDataToWrite[key] = localeFileData[key];
       });
 
       // Write back to the locale JSON file with our updated data
       await fs.promises.writeFile(
         localeFilePath,
-        JSON.stringify(localeFileData, null, "\t")
+        JSON.stringify(localeDataToWrite, null, "\t")
       );
-      console.log(`Update locale file at ${localeFilePath}`);
+      console.log(`Updated locale file at ${localeFilePath}`);
     })
   );
 })();
