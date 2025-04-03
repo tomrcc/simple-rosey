6b01186a9b2eb3eeb1d559a1bff001c5b0368735
tomrcc <tom@cloudcannon.com>
2025-04-03 14:26:24 +1300

    Fix base url, lang picker styles

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,14 +1,14 @@
-import site from '../../data/site.json';
-import { getCollection } from 'astro:content';
+import site from "../../data/site.json";
+import { getCollection } from "astro:content";
 
-import rss from '@astrojs/rss';
-const posts = await getCollection('blog');
+import rss from "@astrojs/rss";
+const posts = await getCollection("blog");
 
 export async function GET() {
   return rss({
     title: site.site_title,
     description: site.description,
-    site: 'https://tiny-jackal.cloudvent.net',
+    site: "https://wonderful-detergent.cloudvent.net",
     items: posts.map((post) => ({
       link: `/blog/${post.slug}`,
       title: post.data.title,
